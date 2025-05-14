4a660784a7d9311929b66d7e6850920e3976ff6a
mawinist10 <mawinist10@gmail.com>
2025-05-14 17:52:44 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-require 'src/Bot.php';
-$bot = new Bot();
-$bot->run();
+$token = getenv('TELEGRAM_BOT_TOKEN');
+$input = file_get_contents('php://input');
+$update = json_decode($input, true);
+
+$chat_id = $update['message']['chat']['id'] ?? null;
+$text = $update['message']['text'] ?? '';
+
+if ($text === '/start' && $chat_id) {
+    $message = "Привет! Я помогу тебе визуализировать комнату с мебелью Globus.";
+    file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($message));
+}
+?>
